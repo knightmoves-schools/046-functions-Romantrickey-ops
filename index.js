0318c017f7a9edfa8298c8f8564d65ab9aaf200e
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-05-18 14:38:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-Square (a, b){
+square multiplied(a, b){
  return a * b;
 }
+let multiplied = square(a, b){
+return a * b;
 
-document.getElementById("result").innerHTML = 
+document.getElementById("result").innerHTML = square(3, 2);
